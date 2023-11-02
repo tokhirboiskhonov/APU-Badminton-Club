6e96d772f114427ed17fdda2152eb0d35de7f687
tokhirboiskhonov <tokhir060@gmail.com>
2023-11-02 19:50:22 +0800

    created blogs-list.js data

---
+++ datas/blogs-list.js
@@ -0,0 +1,43 @@
+const blogs = [
+  {
+    image: "./assets/images/blog-1.jpg",
+    dateOfPublishing: "13 Jun",
+    timeReader: "2 min Read",
+    descriptionLink: "Get excellent result from the recent competition",
+  },
+
+  {
+    image: "./assets/images/blog-2.jpg",
+    dateOfPublishing: "23 July",
+    timeReader: "7 min Read",
+    descriptionLink: "Friendly match with others university",
+  },
+
+  {
+    image: "./assets/images/blog-2.jpg",
+    dateOfPublishing: "23 July",
+    timeReader: "7 min Read",
+    descriptionLink: "Friendly match with others university",
+  },
+
+  {
+    image: "./assets/images/blog-2.jpg",
+    dateOfPublishing: "23 July",
+    timeReader: "7 min Read",
+    descriptionLink: "Friendly match with others university",
+  },
+
+  {
+    image: "./assets/images/blog-2.jpg",
+    dateOfPublishing: "23 July",
+    timeReader: "7 min Read",
+    descriptionLink: "Friendly match with others university",
+  },
+
+  {
+    image: "./assets/images/blog-2.jpg",
+    dateOfPublishing: "23 July",
+    timeReader: "7 min Read",
+    descriptionLink: "Friendly match with others university",
+  },
+];
