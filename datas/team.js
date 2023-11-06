7cf21bafae2e7f848dff3d86cafb6e6940d52561
tokhirboiskhonov <tokhir060@gmail.com>
2023-11-07 01:56:42 +0800

    changed from Teacher js file to team js file.

---
+++ datas/team.js
@@ -0,0 +1,102 @@
+const teams = [
+  {
+    name: "Mr. Chen Zi Rong",
+    job: "Badminton Coach",
+    description:
+      "I'm excited to welcome you to the Asia Pacific University Badminton Club, where your badminton journey is about to reach new heights. As your Badminton Coach, I'm not just here to teach techniques; I'm here to be your partner, motivator, and guide on this exhilarating adventure.",
+    image: "./assets/images/zirong-coach.jpg",
+    socials: [
+      {
+        type: "telegram",
+        link: "https://t.me",
+      },
+      {
+        type: "instagram",
+        link: "https://instagram.com",
+      },
+      {
+        type: "facebook",
+        link: "https://facebook.com",
+      },
+      {
+        type: "twitter",
+        link: "https://twitter.com",
+      },
+    ],
+  },
+  {
+    name: "Mr. Maxim Monastyrskiy",
+    job: "Fitness Coach",
+    description:
+      "I am excited to welcome you to the Asia Pacific University Badminton Club’s fitness circle, where the journey toward your best self begins. As your Fitness Coach, I’m not just here to craft workout routines; I’m here to be your partner, motivator, and confidant on this transformative adventure.",
+    image: "./assets/images/maxim-pic.jpg",
+    socials: [
+      {
+        type: "telegram",
+        link: "https://t.me",
+      },
+      {
+        type: "instagram",
+        link: "https://instagram.com",
+      },
+      {
+        type: "facebook",
+        link: "https://facebook.com",
+      },
+      {
+        type: "twitter",
+        link: "https://twitter.com",
+      },
+    ],
+  },
+  {
+    name: "Mr. Darryl Leong",
+    job: "Chairman",
+    description:
+      "I am honoured to welcome you to the virtual home of the Asia Pacific University Badminton Club. As the Chairman, it fills me with immense pride to introduce you to a vibrant community of players, enthusiasts, and champions in the making. Here at the Asia Pacific University Badminton Club, we don't just play the sport; we embody its spirit and live its values.",
+    image: "./assets/images/darryl-pic.jpg",
+    socials: [
+      {
+        type: "telegram",
+        link: "https://t.me",
+      },
+      {
+        type: "instagram",
+        link: "https://instagram.com",
+      },
+      {
+        type: "facebook",
+        link: "https://facebook.com",
+      },
+      {
+        type: "twitter",
+        link: "https://twitter.com",
+      },
+    ],
+  },
+  {
+    name: "Mr. Boiskhonov Tokhirkhuja",
+    job: "Manager",
+    description:
+      "I extend a warm welcome to all badminton enthusiasts on behalf of the Asia Pacific University Badminton Club. As the Club Manager, my team and I specialize in meticulously planning and executing competitions and events.",
+    image: "./assets/images/tokhirkhuja.jpg",
+    socials: [
+      {
+        type: "telegram",
+        link: "https://t.me",
+      },
+      {
+        type: "instagram",
+        link: "https://instagram.com",
+      },
+      {
+        type: "facebook",
+        link: "https://facebook.com",
+      },
+      {
+        type: "twitter",
+        link: "https://twitter.com",
+      },
+    ],
+  },
+];
